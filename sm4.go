a37882758cdcb8cf3550caaea41d20cc27fe1431
BeDreamCoder <tailinzhang1993@gmail.com>
2020-07-26 00:42:14 +0800

    refine sm4

--- sm4.go
+++ sm4.go
@@ -7,72 +7,67 @@ SPDX-License-Identifier: Apache-2.0
 package gm_ossl
 
 import (
-	"bytes"
+	ci "crypto/cipher"
+	"fmt"
 
 	"github.com/pkg/errors"
 )
 
-type SM4Cipher interface {
-	Cipher
-	Encrypt(plaintext []byte) (ciphertext []byte, err error)
-	Decrypt(ciphertext []byte) (plaintext []byte, err error)
-}
+type SM4Cipher struct{}
 
 type sm4Cipher struct {
 	Cipher
 	key []byte
 }
 
-func NewSM4Cipher(cipherName string, key []byte) (SM4Cipher, error) {
-	if cipherName == "" {
-		cipherName = "sm4-cbc"
-	}
-	cipher, err := GetCipherByName(cipherName)
+func (sc *SM4Cipher) NewSm4Cipher(key []byte) (ci.Block, error) {
+	cipher, err := GetCipherByName("sm4-cbc")
 	if err != nil {
 		return nil, errors.Errorf("Could not get cipher: %s", err.Error())
 	}
 	return &sm4Cipher{cipher, key}, nil
 }
 
-func (s *sm4Cipher) Encrypt(plaintext []byte) ([]byte, error) {
+func (s *sm4Cipher) BlockSize() int {
+	return s.Cipher.BlockSize()
+}
+
+func (s *sm4Cipher) Encrypt(ciphertext, plaintext []byte) {
 	encCtx, err := s.NewEncryptionCipher(nil, s.key, nil)
 	if err != nil {
-		return nil, errors.Errorf("Could not create encryption context: %s", err.Error())
+		panic(fmt.Sprintf("Could not create encryption context: %s", err.Error()))
 	}
 
-	cipherbuffer := new(bytes.Buffer)
 	cipherbytes, err := encCtx.Update(plaintext)
 	if err != nil {
-		return nil, errors.Errorf("Encrypt Update plaintext: %s failure: %s", string(plaintext), err.Error())
+		panic(fmt.Sprintf("Encrypt Update plaintext: %s failure: %s", string(plaintext), err.Error()))
 	}
-	cipherbuffer.Write(cipherbytes)
+	dataSize := len(cipherbytes)
+	copy(ciphertext[:dataSize], cipherbytes)
 
 	cipherbytes, err = encCtx.Final()
 	if err != nil {
-		return nil, errors.Errorf("Encrypt Final failure: %s", err.Error())
+		panic(fmt.Sprintf("Encrypt Final failure: %s", err.Error()))
 	}
-	cipherbuffer.Write(cipherbytes)
-
-	return cipherbuffer.Bytes(), nil
+	copy(ciphertext[dataSize:], cipherbytes)
 }
 
-func (s *sm4Cipher) Decrypt(ciphertext []byte) ([]byte, error) {
+func (s *sm4Cipher) Decrypt(plaintext, ciphertext []byte) {
 	decCtx, err := s.NewDecryptionCipher(nil, s.key, nil)
 	if err != nil {
-		return nil, errors.Errorf("Could not create decryption context: %s", err.Error())
+		panic(fmt.Sprintf("Could not create decryption context: %s", err.Error()))
 	}
-	plainbuffer := new(bytes.Buffer)
+
 	plainbytes, err := decCtx.Update(ciphertext)
 	if err != nil {
-		return nil, errors.Errorf("Decrypt Update ciphertext failure: %s", err.Error())
+		panic(fmt.Sprintf("Decrypt Update ciphertext failure: %s", err.Error()))
 	}
-	plainbuffer.Write(plainbytes)
+	dataSize := len(plainbytes)
+	copy(plaintext[:dataSize], plainbytes)
 
 	plainbytes, err = decCtx.Final()
 	if err != nil {
-		return nil, errors.Errorf("Decrypt Final failure: %s", err.Error())
+		panic(fmt.Sprintf("Decrypt Final failure: %s", err.Error()))
 	}
-	plainbuffer.Write(plainbytes)
-
-	return plainbuffer.Bytes(), nil
+	copy(plaintext[dataSize:], plainbytes)
 }
--- sm4_test.go
+++ sm4_test.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package gm_ossl
 
 import (
+	"bytes"
 	"crypto/rand"
 	"io"
 	"testing"
@@ -17,47 +18,50 @@ import (
 var key = []byte("1234567890abcdef")
 
 func TestSM4(t *testing.T) {
-	sc, err := NewSM4Cipher("", key)
+	s4 := new(SM4Cipher)
+	sc, err := s4.NewSm4Cipher(key)
 	assert.NoError(t, err)
 
 	plaintextIn := []byte("testing sm4 cipher")
-	ciphertext, err := sc.Encrypt(plaintextIn)
-	assert.NoError(t, err)
 
-	plaintextOut, err := sc.Decrypt(ciphertext)
-	assert.NoError(t, err)
+	padding := len(plaintextIn) % sc.BlockSize()
+	nSize := sc.BlockSize() - padding
+	inDataVLen := len(plaintextIn) + nSize
 
-	assert.Equal(t, plaintextIn, plaintextOut)
+	ciphertext := make([]byte, inDataVLen)
+	sc.Encrypt(ciphertext, plaintextIn)
+
+	plaintextOut := make([]byte, len(ciphertext))
+	sc.Decrypt(plaintextOut, ciphertext)
+
+	actualPlaintext := bytes.TrimRight(plaintextOut, "\x00")
+
+	assert.Equal(t, plaintextIn, actualPlaintext)
 }
 
-func BenchmarkSM4Encrypt(b *testing.B) {
+func BenchmarkSM4Crypt(b *testing.B) {
 	b.ReportAllocs()
-	sc, _ := NewSM4Cipher("", key)
+	s4 := new(SM4Cipher)
+	sc, err := s4.NewSm4Cipher(key)
+	if err != nil {
+		b.Fatal(err)
+	}
 
-	var length int64 = 1024 * 1024
+	length := 1024 * 1024
 	buf := make([]byte, length)
 	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
 		b.Fatal(err)
 	}
-	b.SetBytes(length)
+	padding := length % sc.BlockSize()
+	nSize := sc.BlockSize() - padding
+	inDataVLen := length + nSize
+
+	b.SetBytes(int64(length*2 + inDataVLen))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = sc.Encrypt(buf)
-	}
-}
-
-func BenchmarkSM4Decrypt(b *testing.B) {
-	b.ReportAllocs()
-	sc, _ := NewSM4Cipher("", key)
-
-	var length int64 = 1024 * 1024
-	buf := make([]byte, length)
-	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
-		b.Fatal(err)
-	}
-	b.SetBytes(length)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _ = sc.Decrypt(buf)
+		in := make([]byte, length)
+		out := make([]byte, inDataVLen)
+		sc.Encrypt(out, buf)
+		sc.Decrypt(in, out)
 	}
 }
